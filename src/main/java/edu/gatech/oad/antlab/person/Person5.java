deac8d3f696de4d57c3ca85feaa3bdcb90ffc26f
shenshaohua <shnice_shen@sina.com>
2018-09-04 20:04:25 -0400

    Implementation for Person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  String two = input.substring(0, 2);
+		String rest = input.substring(2, input.length());
+		String output = rest + two;
+	  return output;
 	}
 	
 	/**
