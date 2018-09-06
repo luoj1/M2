b81cf6da9c2a3324e9c77f1065a053277b4970dd
shenshaohua <shnice_shen@sina.com>
2018-09-06 18:43:05 -0400

    Information of person5

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Shaohua Shen");
+                  toPrint += p5.toString("sshen76");
 		  
         System.out.println(toPrint);
         
